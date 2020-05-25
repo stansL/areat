1a051ad06441596c06d330eb23a2af0e2a9b6025
Stanslaus <mstans8@gmail.com>
2020-05-25 12:06:05 +0300

    add functionality to fetch individual record with useEffect dependency on id

--- src/components/hooksv2/FetchData.js
+++ src/components/hooksv2/FetchData.js
@@ -3,27 +3,33 @@ import axios from 'axios';
 
 function FetchData() {
 
-    const [posts, setPosts] = useState([]);
+    const [id, setId] = useState(1);
+
+
+    const [post, setPost] = useState({});
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => {
-                console.log(res)
-                setPosts(res.data)
+                console.log(res.data)
+                setPost(res.data)
             })
             .catch(err => console.log(err))
         // return () => {
         //     cleanup
         // }
-        }, [])
+    }, [id])
     // });
     return (
         <div>
-            <ul>
+            <input type="number" value={id} onChange={e => setId(e.target.value)} /> <br />
+            {/* <ul>
                 {posts.map(post => (
-                <li key={post.id}> {post.title}</li>
+                    <li key={post.id}> {post.title}</li>
                 ))}
-            </ul>
+            </ul> */}
+
+            {post.title}
 
         </div>
     )
