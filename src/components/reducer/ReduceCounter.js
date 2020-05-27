7e3e0467b2dbea925a958bc9e9ddbc343f7fb112
Stanslaus <mstans8@gmail.com>
2020-05-27 20:10:34 +0300

    add useReducer functionality wit complex state and action

--- src/components/reducer/ReduceCounter.js
+++ src/components/reducer/ReduceCounter.js
@@ -1,7 +1,11 @@
 import React, { useReducer } from 'react'
 
 
-const defaultState = 0;
+const defaultState = {
+    // multiple state variables, remember the spread
+    firstCounter: 0,
+    secondCounter:10
+};
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 const RESET = 'RESET';
@@ -9,10 +13,10 @@ const RESET = 'RESET';
 const reducer = (state = defaultState, action) => {
     switch (action.type) {
         case INCREMENT: {
-            return state + 1;
+            return { firstCounter: state.firstCounter + action.value };
         }
         case DECREMENT: {
-            return state - 1;
+            return { firstCounter: state.firstCounter - action.value };
         }
         case RESET: {
             return defaultState;
@@ -27,10 +31,14 @@ function ReduceCounter() {
     const [count, dispatch] = useReducer(reducer, defaultState)
     return (
         <div>
-            <div>Count : {count}</div>
-            <button onClick={() => dispatch({ type: INCREMENT })}>Increment</button>
-            <button onClick={() => dispatch({ type: DECREMENT })}>Decrement</button>
+            <div>Count : {count.firstCounter}</div>
+            {/* multiple action properties */}
+            <button onClick={() => dispatch({ type: INCREMENT, value: 1 })}>Increment</button>
+            <button onClick={() => dispatch({ type: DECREMENT, value: 1 })}>Decrement</button>
+            <button onClick={() => dispatch({ type: INCREMENT, value: 5 })}>Incr by 5</button>
+            <button onClick={() => dispatch({ type: DECREMENT, value: 5 })}>Decr by 5</button>
             <button onClick={() => dispatch({ type: RESET })}>Reset</button>
+
         </div>
     )
 }
